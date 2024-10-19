7878c6a8adda9b36d0c94a3c69975a0e24bf68b2
Fstack-MdAltaf <altaf.fstack@gmail.com>
2024-10-19 16:36:10 +0530

    for empty string input return zero

---
+++ index.js
@@ -0,0 +1,6 @@
+const getSumOfStr = (str) => {
+  let sum = 0;
+  if (str === "" || str.length === 0) return sum;
+};
+
+module.exports = getSumOfStr;
--- index.test.js
+++ index.test.js
@@ -1,3 +1,9 @@
-describe(() => {
-  test(() => {});
+const getSumOfStr = require("./index");
+
+describe("getSumOfStr", () => {
+  test("returns 0 for an empty string", () => {
+    const str = "";
+    const response = getSumOfStr(str);
+    expect(response).toBe(0);
+  });
 });
